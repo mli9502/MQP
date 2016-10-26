cae971f1bc37d01806eeae8266964e516a599f88
mli <mli9502@outlook.com>
2016-10-26 15:23:25 -0400

    Fix SerialPort reading progrem.

--- SerialPort/SerialPort/SerialPort.cpp
+++ SerialPort/SerialPort/SerialPort.cpp
@@ -9,20 +9,16 @@
 
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	printf("Welcome to the serial test app!\n\n");
-
+int main(int argc, _TCHAR* argv[]) {
 	// Serial* SP = new Serial("\\\\.\\COM10");    // adjust as needed
 	Serial* SP = new Serial("\\\\.\\COM3");
 	if (SP->IsConnected())
 		printf("Connected\n");
 	char incomingData[256] = "";			// don't forget to pre-allocate memory
-											//printf("%s\n",incomingData);
+	// printf("%s\n",incomingData);
 	int dataLength = 255;
 	int readResult = 0;
-	while (SP->IsConnected())
-	{
+	while (SP->IsConnected()) {
 		readResult = SP->ReadData(incomingData, dataLength);
 		// printf("Bytes read: (0 means no data available) %i\n",readResult);
 		incomingData[readResult] = 0;
--- SerialPort/ardSendMsg/ardSendMsg.ino
+++ SerialPort/ardSendMsg/ardSendMsg.ino
@@ -4,12 +4,15 @@ void setup() {
 
 void loop() {
     int bytesSent = Serial.write("Hello ");
-    int time = millis();
+    unsigned long time = millis();
     String timeStr = String(time);
-    int arrLen = timeStr.length();
-    char timeArr[arrLen];
-    timeStr.toCharArray(timeArr, arrLen);
-    Serial.write(timeArr);
+    writeString(timeStr);
     Serial.write("\n");
     delay(1000);
 }
+void writeString(String stringData) { // Used to serially push out a String with Serial.write()
+  for (int i = 0; i < stringData.length(); i++)
+  {
+      Serial.write(stringData[i]);   // Push each char 1 by 1 on each loop pass
+  }
+}// end writeString
